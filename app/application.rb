5794d3ed43844df62f7dd5e0987bdf5e61576d92
4myc <missch3n@gmail.com>
2019-12-07 18:17:02 -0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,16 +7,16 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    # if req.path.match(/items/)
-    #   @@items.each do |item|
-    #     resp.write "#{item}\n"
-    #   end
-    # elsif req.path.match(/search/)
-    #   search_term = req.params["q"]
-    #   resp.write handle_search(search_term)
-    # else
-    #   resp.write "Path Not Found"
-    # end
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    else
+      resp.write "Path Not Found"
+    end
 
     if req.path.match(/cart/)
       if @@cart.empty?
